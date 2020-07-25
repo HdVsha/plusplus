72ace0fae62b2b7b12e8ec9c97b34806dca656ed
HdVsha <62552291+HdVsha@users.noreply.github.com>
2020-07-25 10:02:47 +0300

    Update istream-ostream.cpp

--- istream-ostream.cpp
+++ istream-ostream.cpp
@@ -1,5 +1,5 @@
 
-// you need to include at least on arg as ostream or istream to be able to create your new operator(from where do you want to take your info?)
+// you need to include at least one arg as ostream or istream to be able to create your new operator(from where do you want to take your info?)
 ostream& operator<<(ostream& out, const argument){ // & - is necessary to be able to continue printing out 
   return out;
 }
